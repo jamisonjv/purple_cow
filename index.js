aea1b18cb5411099c9d62e3cf6a417d878d42ee7
JJ Vulopas <jamison.vulopas@lucidma.com>
2022-10-19 15:08:57 -0400

    need some boilerplate body parsing ware on initializing express so can pass a json object array to the replace all endpoint

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // establish express server
-const app = require('express')();
+const express = require('express');
+const app = express();
+// need the below so can parse body of requests
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 
 // establish items endpoint
 app.use('/items', require('./api/items'));
